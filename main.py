468ba38c7ca62fbb78f2a7af52684db46dbe0784
AstrixGame <78021940+astrixgame@users.noreply.github.com>
2024-05-22 19:20:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from pygame import mixer
-from time import sleep
-import io, sys, requests
+import io, sys, requests, time, pygame
 
 if len(sys.argv[1:]) > 0:
     r = requests.post("https://ttsmp3.com/makemp3_new.php", data={'msg': sys.argv[1:][0], 'lang': 'Matthew', 'source': 'ttsmp3'})
@@ -8,8 +6,8 @@ if len(sys.argv[1:]) > 0:
     if rj['Error'] == 0:
         res = requests.get(rj['URL'])
         res.raise_for_status()
-        mixer.init()
-        mixer.music.load(io.BytesIO(res.content))
-        mixer.music.play()
-        while mixer.music.get_busy():
-            sleep(1)
+        pygame.mixer.init()
+        pygame.mixer.music.load(io.BytesIO(res.content))
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            time.sleep(1)
