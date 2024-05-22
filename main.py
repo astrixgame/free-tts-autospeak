fda4b775daec7558b4dc3b8d99ea39fa231800c9
AstrixGame <78021940+astrixgame@users.noreply.github.com>
2024-05-22 19:18:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-import requests
 from pygame import mixer
 from time import sleep
-import io
+import io, sys, requests
 
-def tts(text):
-    response = requests.post("https://ttsmp3.com/makemp3_new.php", data={'msg': text, 'lang': 'Matthew', 'source': 'ttsmp3'})
-    response_json = response.json()
-    if response_json['Error'] == 0:
-        response = requests.get(response_json['URL'])
-        response.raise_for_status()
+if len(sys.argv[1:]) > 0:
+    r = requests.post("https://ttsmp3.com/makemp3_new.php", data={'msg': sys.argv[1:][0], 'lang': 'Matthew', 'source': 'ttsmp3'})
+    rj = r.json()
+    if rj['Error'] == 0:
+        res = requests.get(rj['URL'])
+        res.raise_for_status()
         mixer.init()
-        mixer.music.load(io.BytesIO(response.content))
+        mixer.music.load(io.BytesIO(res.content))
         mixer.music.play()
         while mixer.music.get_busy():
             sleep(1)
-
-tts("The answer to the universe is 42")
